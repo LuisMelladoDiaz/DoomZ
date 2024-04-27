45c4b645ac6d4c4252fd291dc7c006e024cc4c16
LuisMelladoDiaz <melladodiazluis@gmail.com>
2024-04-27 14:02:10 +0200

    feat(Player): Movement

---
+++ player/movement.py
@@ -0,0 +1,97 @@
+import math
+from player.player import P
+
+
+class Keys:
+    def __init__(self):
+        self.w = 0
+        self.s = 0
+        self.a = 0
+        self.d = 0
+        self.sl = 0
+        self.sr = 0
+        self.m = 0
+
+K = Keys()
+
+
+# Function to move the player
+def movePlayer():
+
+    if K.a == 1 and K.m == 0:
+        P.horizontal_angle -= 0.05
+        # if P.horizontal_angle < 0:
+            # P.horizontal_angle += 360
+
+    if K.d == 1 and K.m == 0:
+        P.horizontal_angle += 0.05
+        # if P.horizontal_angle > 359:
+            # P.horizontal_angle -= 360
+
+    dx = math.sin(P.horizontal_angle)*10.0
+    dy = math.cos(P.horizontal_angle)*10.0
+
+    if K.w == 1 and K.m == 0:
+        P.x += dx
+        P.y += dy
+
+    if K.s == 1 and K.m == 0:
+        P.x -= dx
+        P.y -= dy
+
+    if K.sr == 1:
+        P.x += dy
+        P.y -= dx
+
+    if K.sl == 1:
+        P.x -= dy
+        P.y += dx
+
+    if K.a == 1 and K.m == 1:
+        P.vertical_angle -= 1
+
+    if K.d == 1 and K.m == 1:
+        P.vertical_angle += 1
+
+    if K.w == 1 and K.m == 1:
+        P.z -=4
+
+    if K.s == 1 and K.m == 1:
+        P.z += 4
+
+
+# Keyboard function for key press
+def KeysDown(key, x, y):
+    global K
+    if key == b'w':
+        K.w = 1
+    if key == b's':
+        K.s = 1
+    if key == b'a':
+        K.a = 1
+    if key == b'd':
+        K.d = 1
+    if key == b'm':
+        K.m = 1
+    if key == b',':
+        K.sr = 1
+    if key == b'.':
+        K.sl = 1
+
+# Keyboard function for key release
+def KeysUp(key, x, y):
+    global K
+    if key == b'w':
+        K.w = 0
+    if key == b's':
+        K.s = 0
+    if key == b'a':
+        K.a = 0
+    if key == b'd':
+        K.d = 0
+    if key == b'm':
+        K.m = 0
+    if key == b',':
+        K.sr = 0
+    if key == b'.':
+        K.sl = 0
---
+++ player/player.py
@@ -0,0 +1,9 @@
+class Player:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.z = 0
+        self.horizontal_angle = 0
+        self.vertical_angle = 0
+
+P = Player()
